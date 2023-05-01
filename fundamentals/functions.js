0deb1da29d30b9153cf2071f053b7c1b2284fcd8
lucascriado <lucaasbrandini@gmail.com>
2023-05-01 17:43:24 -0300

    organizing the fundamentals files - fuctions.js 2

--- fundamentals/functions.js
+++ fundamentals/functions.js
@@ -7,3 +7,25 @@ function nameFuction (a) {
 
 variavel = nameFuction
 console.log(nameFuction())
+
+let anom = function () {
+     return 'Sou o retorno de uma função!'
+}
+
+console.log(anom)
+
+console.log(anom())
+
+/* exemplo de arrow function, de forma muito mais otimizada */
+
+let arrowF = () => "this a return." 
+
+/* função imediata ou exemplo de IIFE */
+
+let returnImed = (function(){
+    return "hello world!"
+})()
+
+console.log(returnImed)
+
+
