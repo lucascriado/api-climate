6af36a106bcad48ecbb54092f766dd776d724329
lucascriado <lucaasbrandini@gmail.com>
2023-04-30 22:51:17 -0300

    organizing the fundamentals files - stringlenght.js

---
+++ fundamentals/stringlenght.js
@@ -0,0 +1,12 @@
+let string1 = "olá"
+let string2 = " mundo!"
+let string12 = string1 + string2
+console.log(string12)
+
+let string6 =  "qual a primeira letra dessa frase "
+let string3 =  "quantos números tem essa frase "
+let string4 = string3.length
+
+console.log(string3 + string4)
+
+let string5 = string3[0]
