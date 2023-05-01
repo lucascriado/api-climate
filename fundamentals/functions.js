e15059745d345efc7f4ef414c7c0f1d1096837ad
lucascriado <lucaasbrandini@gmail.com>
2023-05-01 13:08:34 -0300

    organizing the fundamentals files - fuctions.js 1

---
+++ fundamentals/functions.js
@@ -0,0 +1,9 @@
+let variavel = "this a string!"
+
+function nameFuction (a) {
+    let num1 = 10, num2 = 10, soma = num1 + num2
+    return 0 
+}
+
+variavel = nameFuction
+console.log(nameFuction())
