b316b8b918423c75c3b822b869c4c26150a36142
lucascriado <lucaasbrandini@gmail.com>
2023-04-30 22:01:59 -0300

    organizing the fundamentals files - primitivetype.js

---
+++ fundamentals/primitivetype.js
@@ -0,0 +1,11 @@
+let number = 10
+let string = "Hello World"
+let bool = true
+let letNull = null 
+let letUndefined = undefined
+
+console.log(`O tipo do número é ${typeof number}`)
+console.log(`O tipo da string é ${typeof string}`)
+console.log(`O tipo do booleano é ${typeof bool}`)
+console.log(`O tipo do nulo é ${typeof letNull}`)
+console.log(`O tipo do indefinido é ${typeof letUndefined}`)
