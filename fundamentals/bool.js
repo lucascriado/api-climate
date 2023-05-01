7077c6d7431ce6095b47e9c205e04fcb229fc0f4
lucascriado <lucaasbrandini@gmail.com>
2023-05-01 11:47:29 -0300

    organizing the fundamentals files - nullundefinied.js

---
+++ fundamentals/bool.js
@@ -0,0 +1,20 @@
+let boolFalse = false;
+let boolTrue = true;
+
+let x = 0 
+let x1 = ""
+let x2 = null
+let x3 = undefined 
+let x4 = NaN
+console.log(!!x, !!x1, !!x2, !!x3, !!x4)
+
+let x5 = false
+let x6 = true
+
+console.log(!x5) 
+console.log(!x6)
+
+console.log(5 > 1)
+console.log(1 > 5)
+console.log(5 == "5")
+console.log(5 === "5")
---
+++ fundamentals/nullundefinied.js
@@ -0,0 +1,10 @@
+/* um dos bugs do ecmaScript */
+typeof null 
+/* valor experado sem esse bug deveria null, porém aparece como objeto */
+console.log(typeof null)
+/* -- */
+
+console.log(typeof undefined)
+
+null == undefined 
+null === undefined
