d15acbb381fd98a34ae80fdb2a5d64a5be4e819f
lucascriado <lucaasbrandini@gmail.com>
2023-04-30 22:42:33 -0300

    organizing the fundamentals files - operations.js

---
+++ fundamentals/operations.js
@@ -0,0 +1,10 @@
+console.log(`sum ${10 + 10}`)
+console.log(`sub ${10 - 10}`)
+console.log(`mult ${10 * 10}`)
+console.log(`div ${10 / 10}`)
+console.log(`sum ${10 % 10}`)
+
+console.log(`sum w/ parseint ${parseInt(10.2 + 10.2)}`)
+
+console.log(`sum w/ parseint ${(10.25555 + 10.233333).toFixed(3)}`)
+
