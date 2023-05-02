4b37e80ce29a30e4c82cfc127296c7872f203593
lucascriado <lucaasbrandini@gmail.com>
2023-05-01 22:11:09 -0300

    organizing the fundamentals files - hoisting.js

---
+++ fundamentals/hoisting.js
@@ -0,0 +1,19 @@
+/* hoisting /=> elevação ou içamento */
+/* o uso de var pode ocasionar no içamento por isso 
+acaba não sendo interessante seyu uso por motivos de segurança */
+
+foo = 20
+console.log(foo)
+var foo
+
+/* erro */
+
+foo1 = 20
+console.log(foo)
+let foo1
+
+/* erro */
+
+foo2 = 20
+console.log(foo)
+const foo2
