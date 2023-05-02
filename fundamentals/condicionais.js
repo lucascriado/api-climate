26415560faa6a2ea6348e849574803117b2eb5c2
lucascriado <lucaasbrandini@gmail.com>
2023-05-01 23:35:34 -0300

    organizing the fundamentals files - condicionais.js

---
+++ fundamentals/condicionais.js
@@ -0,0 +1,15 @@
+let fruit = "apple"
+
+if(fruit === "apple"){
+    console.log("fruit ok")
+} else {
+    console.log("fruit not ok")
+}
+
+fruit = "applee"
+
+if(fruit === "apple"){
+    console.log("fruit ok")
+} else {
+    console.log("fruit not ok")
+}
