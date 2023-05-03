040098f19bba78e78adf80028e2e5c811893016a
lucascriado <lucaasbrandini@gmail.com>
2023-05-03 17:17:00 -0300

    organizing the fundamentals files - operatorternario.js

---
+++ fundamentals/operatorternario.js
@@ -0,0 +1,9 @@
+const idade = 20;
+
+idade >= 18 ? console.log("tem idade maior eim!") : console.lo("não tem idade maior :(")
+
+const sumMult = (valor) => {
+    return valor >= 10 ? valor + valor : valor * valor 
+}
+
+console.log(sumMult(2))
