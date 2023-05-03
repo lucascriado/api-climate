76e5320b1a05f55db1c25e57f2493139ed6646b3
lucascriado <lucaasbrandini@gmail.com>
2023-05-03 16:49:27 -0300

    organizing the fundamentals files - condicionais.js

--- fundamentals/condicionais.js
+++ fundamentals/condicionais.js
@@ -33,4 +33,40 @@ if(fruit === "apple"){
 }
 
 
-// conectivos && / ||
+// conectivos && / ||/* 
+/* quando temos o conectivo 'ou' pode ter uma verdadeira para ser verdadeiro 
+quando temos o conectivo 'e' tem que ser todas verdadeiras */ 
+
+const num1 = 10,
+num2 = 20
+
+if((num1 === 10) && (num2 == 20))
+    /* nesse caso é esperado que de certo por conta que ambas são verdadeiras */
+    console.log('deu certo!')
+else {
+    console.log('deu errado!')
+}
+
+const num3 = 5, 
+num4 = 3
+
+if((num3 === 10) && (num4 == 20))
+    /* nesse caso é esperado que de false por conta que ambas não são verdadeiras */
+    console.log('deu certo!')
+else {
+    console.log('deu errado!')
+}
+
+const num7 = 1, num8 = 0
+if((num7 === 1) || (num7 === 1)) {
+    console.log("deu certo!")
+} else {
+    console.log('deu errado!')
+}
+
+const num9 = 0, num10 = 0
+if((num9 === 1) || (num10 === 1)) {
+    console.log("deu certo!")
+} else {
+    console.log('deu errado!')
+}
