7297bfc1d4efbcb723321f01eee3d7f1380180d0
lucascriado <lucaasbrandini@gmail.com>
2023-05-22 23:48:54 -0300

    uploading the learning files

--- fundamentals/object.js
+++ fundamentals/object.js
@@ -1,5 +1,19 @@
+/* dot object */
+
 const objectNull = {
     prop1: "olá mundo, eu sou uma propriedade!",
-    function1 = () => "isso é um retorno!"
+    metodo1: teste1 = () => "isso é um retorno!"
 }
 
+console.log(objectNull.prop1)
+console.log(objectNull.metodo1())
+
+/* bracket object */
+
+const objectOther = {
+    prop2: "olá mundo, eu sou uma propriedade!",
+    metodo2: teste1 = () => "isso é um retorno!"
+}
+
+console.log(objectOther['prop2'])
+console.log(objectOther['metodo2']())
