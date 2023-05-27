d41f4ea523085cbb92a3923cd9d3069119d9ebd4
lucascriado <lucaasbrandini@gmail.com>
2023-05-26 21:21:39 -0300

    uploading files over array

---
+++ fundamentals/arrray.js
@@ -0,0 +1,13 @@
+let fruits = []
+
+let fruits1 = [
+    'Maça',
+    'Banana'
+]
+
+console.log(fruits1.length)
+
+let fruits3 = new Array('Lucas1', 'Lucas2')
+
+console.log(fruits3[0])
+console.log(fruits3.length)
