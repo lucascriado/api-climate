496fc6f4be221e9efbafb9b35e71dc4490c94efa
lucascriado <lucaasbrandini@gmail.com>
2023-05-27 23:41:19 -0300

    upload files

--- fundamentals/arrray.js
+++ fundamentals/arrray.js
@@ -11,3 +11,16 @@ let fruits3 = new Array('Lucas1', 'Lucas2')
 
 console.log(fruits3[0])
 console.log(fruits3.length)
+
+/* array denso */
+let fruit4 = new Array('1','2','3')
+
+/* array esparso */
+let fruit5 = new Array('1','','','','5')
+
+let fruit10 = new Array('teste1','teste2', 'teste3')
+console.log(fruit10)
+let del10 = delete fruit10[1]
+
+console.log(fruit10)
+
