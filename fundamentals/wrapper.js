f362638ee3f99fadce89ce92b7de91aac928b4ed
lucascriado <lucaasbrandini@gmail.com>
2023-05-30 18:46:18 -0300

    upload files to wrapper

---
+++ fundamentals/wrapper.js
@@ -0,0 +1,16 @@
+let objlist = ( 'test1', 'test2', 'test3' )
+
+console.log(objlist.length)
+
+teste7 = objlist.split('')
+
+console.log(teste7.length)
+
+console.log(teste7)
+
+const str1 = String("Teste 1")
+const str2 = new String("Teste 2")
+console.log(str1.length)
+console.log(str2.length)
+console.log(`str ${str1} str2 ${str2}`) 
+
