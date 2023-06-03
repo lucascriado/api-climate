67afe1210aeaa5f539dddc1b14fe0b74b296d590
lucascriado <lucaasbrandini@gmail.com>
2023-06-03 13:09:46 -0300

    learning about array methods, an array with filter

--- fundamentals/array3.js
+++ fundamentals/array3.js
@@ -1,4 +1,6 @@
-arrayBundle = [ 'b', 'c', 'a', 'd', 'g', 'y']
+/* ordenando uma array com números */
+
+arrayBundle = [ 1, 4, 5, 6, 2, 3, 90, 30, 21, 10]
 
 let arraylist = arrayBundle.sort((a, b) => a-b)
 
---
+++ fundamentals/arrray4.js
@@ -0,0 +1,14 @@
+/* array filter, find, foreach, map, reduce */
+
+const arrayBundle = [
+    {a: 2, b: 2},
+    {a: 1, b: 2},
+    {a: 5, b: 2},
+    {a: 1, b: 2},
+    {a: 10, b: 2}
+]
+
+/* filter array */
+const filtrando = arrayBundle.filter(x => x.a > 2)
+
+console.log(filtrando)
