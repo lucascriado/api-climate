71fb61862a724561accd017c02f98325dbef8624
lucascriado <lucaasbrandini@gmail.com>
2023-07-28 21:41:42 -0300

    feat: learning about conditions

--- fundamentals/rocket-rascnh2.js
+++ fundamentals/rocket-rascnh2.js
@@ -13,3 +13,29 @@ let h = 'sjrp'
 const value = new Person(g, f, h)
 
 console.log(value)
+
+let phrase = "Eu gosto de programar"
+
+let bundle = phrase.split(" ")
+
+console.log(bundle.includes("Eu"))
+
+console.log(bundle)
+
+let x = bundle.join(" ")
+
+console.log(x)
+
+let array = new Array(10)
+
+console.log(array.length)
+
+// metodos de array 
+
+let phrase2 = 'lucas'
+
+console.log(Array.from(phrase2))
+
+let pack = Array.from("ola")
+
+
---
+++ fundamentals/rocket-rascnh3.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'lucas',
+    age: 13
+}
+
+delete person.name
+
+console.log(person)
+
+const vlr = true 
+
+const vlrup = !!vlr
+
+console.log(vlrup)
