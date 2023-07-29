671a4dfa5520e6a9aa6e1875025a3af19dcd21bc
lucascriado <lucaasbrandini@gmail.com>
2023-07-29 10:30:32 -0300

    feat: learning about for in and for of

--- fundamentals/rocket-rascnh3.js
+++ fundamentals/rocket-rascnh3.js
@@ -27,3 +27,37 @@ try {
 } catch (e) {
     console.log(e)
 }
+
+for(i=0; i<=10; i++){
+    if(true){
+        console.log(`o numero agora é ${i}`)
+    }else{
+        console.log('algo de errado')
+    }
+}
+
+const objxr = {
+    prop1 : "banana",
+    prop2 : "maça",
+    prop3 : "fruta",
+}
+
+const arrxr = [
+    "banana",
+    "maça",
+    "legume",
+]
+
+for(let receive of arrxr){
+    console.log(receive)
+}
+
+for(let rec in objxr){
+    console.log(rec)
+    console.log(objxr[rec])
+}
+
+
+
+
+
